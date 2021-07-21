c05558a7d49bff632bd59ed220af1716dfebb641
Theophile DELMAS <thdelmas@e2r1p14.42.fr>
2021-07-21 18:11:29 +0200

    test read write

--- tests/test_ft_read.c
+++ tests/test_ft_read.c
@@ -1,12 +1,24 @@
 
 #include "libasm.h"
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+
+extern int errno ;
 
 int main(int ac, char **av)
 {
 	char	c;
+	int errnum;
 
-	while (ft_read(0, &c, 1) >= 0)
+	fprintf(stderr, "Value of errno: %d\n", errno);
+	perror("Error printed by perror");
+	fprintf(stderr, "Error opening file: %s\n", strerror( errnum ));
+	while (read(0, NULL, 1) >= 0)
 		printf("%c", c);
 	printf("\n");
+	fprintf(stderr, "Value of errno: %d\n", errno);
+	perror("Error printed by perror");
+	fprintf(stderr, "Error opening file: %s\n", strerror( errnum ));
 	return (0);
 }
--- tests/test_ft_write.c
+++ tests/test_ft_write.c
@@ -1,11 +1,22 @@
-
 #include "libasm.h"
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+
+extern int errno ;
 
 int main(int ac, char **av)
 {
+	char	c;
+	int errnum;
+
+	fprintf(stderr, "\nValue of errno: %d\n", errno);
+	perror("Error printed by perror");
+	fprintf(stderr, "Error opening file: %s\n", strerror( errnum ));
 	if (ac > 1)
-	{
-		ft_write(2, av[1], strlen(av[1]));
-	}
+		ft_write(1, av[1], -1);
+	fprintf(stderr, "\nValue of errno: %d\n", errno);
+	perror("Error printed by perror");
+	fprintf(stderr, "Error opening file: %s\n", strerror( errnum ));
 	return (0);
 }
